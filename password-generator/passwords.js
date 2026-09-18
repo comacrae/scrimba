40578e7e5873d158a0f7f23f91a02a626c7d47cf
Colin MacRae <76566137+comacrae@users.noreply.github.com>
2026-09-18 11:32:33 -0500

    refactored logic into a passwords class

--- password-generator/passwords.js
+++ password-generator/passwords.js
@@ -1,57 +1,62 @@
-const charset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-//init elements
-const generateBtn = document.getElementById("generate-btn");
-console.log(generateBtn);
-const passwordBtns = document.querySelectorAll(".btn-password");
-addEventListeners();
-render();
-
-
-function clearPasswordText(){
-    for(const btn of passwordBtns){
+class Password {
+     static #CHARSET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+        "/"];
+    static OBFUSCATOR = "●";
+    #password = "";
+    obfuscated = "";
+    btn = null;
+    
+    constructor(btn){
         btn.textContent = "";
+         btn.addEventListener("click", this.copyPassword);
+         //btn.addEventListener("hover", showPassword);
+         this.btn = btn;
+         
     }
+    
+    
+    #getChar(){
+        return Password.#CHARSET[Math.floor(Math.random() * Password.#CHARSET.length)];
+
+    }
+    
+    createPassword(passLen=10, useSpecial=true){
+        let password = "";
+        for(let i = 0; i < passLen; i++){
+            password+= this.#getChar();
+        }
+        this.#password = password;
+        this.obfuscated = "";
+        for(let i = 0; i < password.length; i++)
+            this.obfuscated += Password.OBFUSCATOR;
+        this.btn.textContent = this.obfuscated;
+    }
+    
+    clearPassword(){
+        this.btn.textContent = "";
+    }
+    
+    copyPassword = () => {
+        navigator.clipboard.writeText(this.#password);
+    }
+    
 }
 
-function addEventListeners(){
-    generateBtn.addEventListener("click",generatePasswords);
-    for (const btn of passwordBtns){
-        btn.addEventListener("click", copyPassword);
-    }
+const passwords = [];
 
+function generatePasswords(passwords) {
+    for (const password of passwords){
+        password.createPassword();
+    }
 }
 
 function render(){
-    clearPasswordText();
-}
-
-function getChar(){
-    return charset[Math.floor(Math.random() * charset.length)];
-
-}
-
-function generatePasswords(){
+    //init elements
+    const generateBtn = document.getElementById("generate-btn");
+    generateBtn.addEventListener("click", generatePasswords);
+    const passwordBtns = document.querySelectorAll(".btn-password");
+    const passwords = [];
     for(const btn of passwordBtns){
-        populatePassword(createPassword(), btn);
+        passwords.push(new Password(btn));
     }
-}
-
-function createPassword(passLen=10, useSpecial=true){
-    let password = "";
-    for(let i = 0; i < passLen; i++){
-        password+= getChar();
-    }
-    console.log(password);
-    return password;
-}
-
-function populatePassword(password,passBtn){
-    passBtn.textContent = password;
-}
-
-function copyPassword(ev){
-    const btn = ev.currentTarget;
-    navigator.clipboard.writeText(btn.textContent);
 }
